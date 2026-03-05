594bb018f3850e6b6094ee038b7d7750979cefed
Zhiheng Wang <76178363+TraceOnSnow@users.noreply.github.com>
2026-03-04 22:30:46 -0600

    Revert "Bootstrap project structure: models, utils, experiments, train script"

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .gnn_model import GNNModel
-from .clip_encoder import CLIPEncoder
-
-__all__ = ["GNNModel", "CLIPEncoder"]
--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_loader import HearthstoneDataLoader
-from .preprocessing import preprocess_card_data
-
-__all__ = ["HearthstoneDataLoader", "preprocess_card_data"]
